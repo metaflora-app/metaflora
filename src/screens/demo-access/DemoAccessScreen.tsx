0ec582a677cfe5ee9d02ba1909563f4b03ace9e5
user <user@Noutbuk-user.local>
2026-01-09 19:51:00 +0300

    refactor: rebuild demo access screen with tailwind classes

--- src/screens/demo-access/DemoAccessScreen.tsx
+++ src/screens/demo-access/DemoAccessScreen.tsx
@@ -1,19 +1,17 @@
 import { useNavigate } from 'react-router-dom';
 
-// Импорт элементов
+// Импорт элементов (используем уже скопированные файлы)
 import bgImage from '../../assets/demo-access-elements/хэдер и подвал.png';
-import bgDots from '../../assets/demo-access-elements/фон точки.png';
 import title from '../../assets/demo-access-elements/что входит в ваш демо-доступ.png';
 import description from '../../assets/demo-access-elements/course-description.png';
 import iconIncluded from '../../assets/demo-access-elements/иконка что включено в демо.png';
-import iconNotIncluded from '../../assets/demo-access-elements/иконка что не включено в демо.png';
 import accessList from '../../assets/demo-access-elements/access-list.png';
 import bigLogo from '../../assets/demo-access-elements/лого большое в экране демо.png';
 import btnContinueBg from '../../assets/demo-access-elements/кнопка продолжить.png';
-import btnContinueText from '../../assets/demo-access-elements/продолжить.png';
+import iconNotIncluded from '../../assets/demo-access-elements/иконка что не включено в демо.png';
+import exitBtn from '../../assets/demo-access-elements/выход.png';
 import btnPayBg from '../../assets/demo-access-elements/кнопка оплатить полный доступ.png';
 import btnPayText from '../../assets/demo-access-elements/оплатить полный доступ.png';
-import exitBtn from '../../assets/demo-access-elements/выход.png';
 import supportBtn from '../../assets/demo-access-elements/Frame 2131330093.png';
 
 // Размеры дизайна
@@ -42,221 +40,106 @@ export const DemoAccessScreen = () => {
           transformOrigin: 'top left',
         }}
       >
-        {/* Background */}
-        <img
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: 1180,
-            height: 2550,
-          }}
-          alt="Background"
-          src={bgImage}
-        />
-
-        {/* Dots background */}
-        <img
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: 1180,
-            height: 2550,
-          }}
-          alt=""
-          src={bgDots}
-        />
-
-        {/* Title - что входит в ваш демо-доступ */}
-        <img
-          style={{
-            position: 'absolute',
-            height: '6.27%',
-            left: '20.68%',
-            top: '20.45%',
-            width: '68.14%',
-          }}
-          alt=""
-          src={title}
-        />
-
-        {/* Description */}
-        <img
-          style={{
-            position: 'absolute',
-            height: '7.84%',
-            left: '20.92%',
-            top: '29.09%',
-            width: '71.61%',
-          }}
-          alt=""
-          src={description}
-        />
-
-        {/* Icon included (left) */}
-        <img
-          style={{
-            position: 'absolute',
-            height: 98,
-            left: 'calc(50% - 487px)',
-            top: 551,
-            width: 98,
-          }}
-          alt=""
-          src={iconIncluded}
-        />
-
-        {/* Access list block */}
-        <img
-          style={{
-            position: 'absolute',
-            height: 861,
-            left: 21,
-            top: 959,
-            width: 1137,
-          }}
-          alt=""
-          src={accessList}
-        />
-
-        {/* Big logo */}
-        <img
-          style={{
-            position: 'absolute',
-            height: '6.27%',
-            left: '8.51%',
-            top: '12.34%',
-            width: '86.44%',
-          }}
-          alt=""
-          src={bigLogo}
-        />
-
-        {/* Button continue bg */}
-        <button
-          onClick={() => navigate('/main')}
-          style={{
-            position: 'absolute',
-            left: 'calc(50% - 441px)',
-            top: 1850,
-            width: 891,
-            height: 139,
-            background: 'transparent',
-            border: 'none',
-            cursor: 'pointer',
-            padding: 0,
-          }}
-        >
+        <main className="bg-[#020101] w-full min-w-[1180px] h-[2550px] relative">
           <img
-            src={btnContinueBg}
-            alt=""
-            style={{ width: '100%', height: '100%' }}
+            className="absolute top-0 left-0 w-[1180px] h-[2550px]"
+            alt="Background"
+            src={bgImage}
           />
-          <img
-            src={btnContinueText}
-            alt=""
-            style={{
-              position: 'absolute',
-              left: '50%',
-              top: '50%',
-              transform: 'translate(-50%, -50%)',
-              width: 250,
-              height: 'auto',
-            }}
-          />
-        </button>
 
-        {/* Icon not included (left) */}
-        <img
-          style={{
-            position: 'absolute',
-            height: 98,
-            left: 'calc(50% - 488px)',
-            top: 793,
-            width: 98,
-          }}
-          alt=""
-          src={iconNotIncluded}
-        />
+          <img
+            className="absolute w-[68.14%] h-[6.27%] top-[20.45%] left-[20.68%]"
+            alt=""
+            src={title}
+          />
 
-        {/* Exit button */}
-        <button
-          onClick={() => navigate(-1)}
-          style={{
-            position: 'absolute',
-            left: 'calc(50% - 465px)',
-            top: 204,
-            width: 81,
-            height: 64,
-            background: 'transparent',
-            border: 'none',
-            cursor: 'pointer',
-            padding: 0,
-          }}
-        >
           <img
-            src={exitBtn}
+            className="absolute w-[71.61%] h-[7.84%] top-[29.09%] left-[20.92%]"
             alt=""
-            style={{ width: '100%', height: '100%' }}
+            src={description}
           />
-        </button>
 
-        {/* Button pay bg */}
-        <button
-          onClick={() => navigate('/pricing')}
-          style={{
-            position: 'absolute',
-            left: 'calc(50% - 441px)',
-            top: 2010,
-            width: 891,
-            height: 139,
-            background: 'transparent',
-            border: 'none',
-            cursor: 'pointer',
-            padding: 0,
-          }}
-        >
           <img
-            src={btnPayBg}
+            className="absolute top-[551px] left-[calc(50.00%_-_487px)] w-[98px] h-[98px]"
             alt=""
-            style={{ width: '100%', height: '100%' }}
+            src={iconIncluded}
           />
-          <img
-            src={btnPayText}
-            alt=""
-            style={{
-              position: 'absolute',
-              left: '50%',
-              top: '50%',
-              transform: 'translate(-50%, -50%)',
-              width: 508,
-              height: 36,
-            }}
-          />
-        </button>
 
-        {/* Support button */}
-        <button
-          onClick={() => window.open('https://t.me/mishchenko_is', '_blank')}
-          style={{
-            position: 'absolute',
-            left: 'calc(50% + 265px)',
-            top: 190,
-            width: 205,
-            height: 78,
-            background: 'transparent',
-            border: 'none',
-            cursor: 'pointer',
-            padding: 0,
-          }}
-        >
           <img
-            src={supportBtn}
+            className="absolute top-[959px] left-[21px] w-[1137px] h-[861px]"
             alt=""
-            style={{ width: '100%', height: '100%' }}
+            src={accessList}
           />
-        </button>
+
+          <img
+            className="absolute w-[86.44%] h-[6.27%] top-[12.34%] left-[8.51%]"
+            alt=""
+            src={bigLogo}
+          />
+
+          {/* Button continue */}
+          <button
+            onClick={() => navigate('/main')}
+            className="absolute top-[1850px] left-[calc(50.00%_-_441px)] w-[891px] h-[139px]"
+            style={{ background: 'transparent', border: 'none', cursor: 'pointer', padding: 0 }}
+          >
+            <img
+              className="w-full h-full"
+              alt=""
+              src={btnContinueBg}
+            />
+          </button>
+
+          <img
+            className="absolute top-[793px] left-[calc(50.00%_-_488px)] w-[98px] h-[98px]"
+            alt=""
+            src={iconNotIncluded}
+          />
+
+          {/* Exit button */}
+          <button
+            onClick={() => navigate(-1)}
+            className="absolute top-[204px] left-[calc(50.00%_-_465px)] w-[81px] h-16"
+            style={{ background: 'transparent', border: 'none', cursor: 'pointer', padding: 0 }}
+          >
+            <img
+              className="w-full h-full"
+              alt=""
+              src={exitBtn}
+            />
+          </button>
+
+          {/* Button pay */}
+          <button
+            onClick={() => navigate('/pricing')}
+            className="absolute top-[2010px] left-[calc(50.00%_-_441px)] w-[891px] h-[139px]"
+            style={{ background: 'transparent', border: 'none', cursor: 'pointer', padding: 0 }}
+          >
+            <img
+              className="w-full h-full"
+              alt=""
+              src={btnPayBg}
+            />
+            <img
+              className="absolute top-[2060px] left-[341px] w-[508px] h-9"
+              alt=""
+              src={btnPayText}
+            />
+          </button>
+
+          {/* Support button */}
+          <button
+            onClick={() => window.open('https://t.me/mishchenko_is', '_blank')}
+            className="absolute top-[190px] left-[calc(50.00%_+_265px)] w-[205px] h-[78px]"
+            style={{ background: 'transparent', border: 'none', cursor: 'pointer', padding: 0 }}
+          >
+            <img
+              className="w-full h-full"
+              alt=""
+              src={supportBtn}
+            />
+          </button>
+        </main>
       </div>
     </div>
   );
