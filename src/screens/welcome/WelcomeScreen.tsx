fe6459f0f9c5c4417c16962066419ddca36233f6
user <user@Noutbuk-user.local>
2026-01-19 03:30:37 +0300

    fix: adjust icon И to exact Figma coordinates

--- src/screens/welcome/WelcomeScreen.tsx
+++ src/screens/welcome/WelcomeScreen.tsx
@@ -519,16 +519,16 @@ export const WelcomeScreen: React.FC = () => {
           width: '214px',
           height: '119px',
         }}>
-          {/* Иконка "i" */}
+          {/* Иконка "И" */}
           <img 
             src={policyInfoIcon}
             alt="info"
             style={{
               position: 'absolute',
-              left: 0,
-              top: 0,
-              width: '41px',
-              height: '41px',
+              left: '18.7916px',
+              top: '2px',
+              width: '4.2084px',
+              height: '22.9616px',
               objectFit: 'contain',
             }}
           />
