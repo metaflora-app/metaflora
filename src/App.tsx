1eccee3e1c7b733dd3b9b9f1b73af2dc5fc0bda3
user <user@Noutbuk-user.local>
2026-01-21 21:00:16 +0300

    fix: increase splash screen duration to 8 seconds
    - Change SplashScreen timeout from 2000ms to 8000ms
    - Remove GlobalLoader component (not needed)
    - All assets load during splash screen display
    - Simple solution: just show first screen longer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
-import { GlobalLoader } from './components/GlobalLoader';
 
 function App() {
-  return (
-    <GlobalLoader>
-      <RouterProvider router={router} />
-    </GlobalLoader>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
